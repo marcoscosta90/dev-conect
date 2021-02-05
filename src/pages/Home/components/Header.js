1174da2298221df88ab9ca0745577a3dd49ebfc5
marcoscosta90 <marcoscosta.dev@gmail.com>
2021-02-05 14:37:54 -0300

    Header

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -3,25 +3,57 @@ import { Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import { Bell } from 'react-feather';
+import Avatar from '@material-ui/core/Avatar';
 
 const useStyles = makeStyles({
-
+    appBar: {
+        boxShadow: 'none'
+    },
+    img: {
+        maxHeight: 55 
+    },
+    grow: {
+        flexGrow: 1
+    },
+    userSection: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    button: {
+        marginRight: 10,
+    },
+    bell: {
+        marginRight: 10
+    }
 })
 
-
 function Header() {
- return (
-    <AppBar>
+    const classes = useStyles();
+ return (   
+    <AppBar position="fixed" color="inherit" className={classes.appBar}>
         <Toolbar>
-          <div>
+            <img src="/images/logo.jpg" alt="logo" className={classes.img} />
+            <div className={classes.grow}></div>
+            <div className={classes.userSection}>
+                <Button color="primary" variant="contained" className={classes.button}>
+                    Novo post
+                </Button>
+                <SvgIcon className={classes.bell}>
+                    <Bell />
+                </SvgIcon>
+                <Avatar alt="Memy Sharp" src="/" />
+            </div>
+          {/* <div>
             <a href="/">Conecta Dev </a>
             <input type="text"></input>
          </div>
           <div>
-            <Button color="primary" variant="contained">Novo post</Button>
+           
             <span>Img1</span>
             <span>Img2</span>
-        </div>
+        </div> */}
         </Toolbar>
     </AppBar>
     )
