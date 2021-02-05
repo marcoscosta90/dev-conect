f4eccefc35c6ff6a86202e6e15f4c884d1a0738e
marcoscosta90 <marcoscosta.dev@gmail.com>
2021-02-05 14:14:49 -0300

    Feed and Navbar

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -1,19 +1,29 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const useStyles = makeStyles({
+
+})
+
 
 function Header() {
-    return (
-    <header className="header">
-        <div className="toolbar">
-            <div>
+ return (
+    <AppBar>
+        <Toolbar>
+          <div>
             <a href="/">Conecta Dev </a>
+            <input type="text"></input>
          </div>
-        <div>
-            <button>Novo post</button>
+          <div>
+            <Button color="primary" variant="contained">Novo post</Button>
             <span>Img1</span>
             <span>Img2</span>
         </div>
-        </div>
-    </header>
+        </Toolbar>
+    </AppBar>
     )
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,34 @@
 import React from 'react'
-import './style.css'
+// import './style.css'
+import { makeStyles } from '@material-ui/core/styles';
+
 import Header from './components/Header'
+import NavBar from './components/NavBar'
+import Feed from './components/Feed'
+
+const useStyles = makeStyles({
+   root: {
+       display: 'flex',
+       flexDirection: 'column'
+   },
+   main: {
+    height: '100vh',
+    display: 'flex',
+    width: '1200px',
+    margin: '0 auto',
+   }
+})
 
 function Home() {
+    const classes = useStyles();
+
     return  (
-    <div>
+    <div className={classes.root}>
         <Header />
-       <main className="main">
-            <div className="navbar">
-                navbar
-            </div>
-            <div className="feed">
-                feed
-            </div>
+        <div className="toolbar"></div>
+        <main className={classes.main}>
+           <NavBar />
+           <Feed />
        </main>
     </div>
     );
