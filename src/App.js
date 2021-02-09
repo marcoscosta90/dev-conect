8454affdc73446c3b1025282105879ad0087a51e
marcoscosta90 <marcoscosta.dev@gmail.com>
2021-02-09 17:28:51 -0300

    react router 6

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import { ThemeProvider } from '@material-ui/core/styles'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
 import theme from './theme'
 
 function App() {
-  const url = window.location.href
+   return (    
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="*" element={<h1>Not found 404</h1> } />
 
-  return (
-   <ThemeProvider theme={theme}>
-     {
-       url === "http://localhost:3000/" ? <Home /> : <SignIn />
-     }
-        
-        
-   </ThemeProvider> 
+          </Routes>    
+        </BrowserRouter>    
+      </ThemeProvider> 
    
   );
 }
