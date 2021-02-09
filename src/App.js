8108580248a4812dc8c2389c1228151b4501da81
marcoscosta90 <marcoscosta.dev@gmail.com>
2021-02-09 15:16:27 -0300

    theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import Home from './pages/Home/index'
+import Home from './pages/Home/index';
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from './theme'
 
 function App() {
   return (
-   <Home></Home>
+   <ThemeProvider theme={theme}>
+        <Home />
+   </ThemeProvider> 
+   
   );
 }
 
